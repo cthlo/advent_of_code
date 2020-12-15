fa78fcd59417096112215947fc61a861a7806beb
cthlo <cthlo@users.noreply.github.com>
2020-12-15 00:39:57 -0500

    day 15

---
+++ 2020/day15_1.py
@@ -0,0 +1,14 @@
+import fileinput
+
+nums = [int(i) for i in next(fileinput.input()).split(",")]
+
+h = {n: i for i, n in enumerate(nums)}
+for i in range(len(nums), 2020):
+    if h[nums[-1]] != i - 1:
+        nums.append(i - 1 - h[nums[-1]])
+        h[nums[i - 1]] = i - 1
+    else:
+        nums.append(0)
+    h.setdefault(nums[-1], i)
+
+print(nums[-1])
