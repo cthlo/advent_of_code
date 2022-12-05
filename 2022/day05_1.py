91157a9f288d693e80652a5e37cd8f5bb8088f9e
cthlo <cthlo@users.noreply.github.com>
2022-12-04 21:55:00 -0800

    2022 day 5

---
+++ 2022/day05_1.py
@@ -0,0 +1,20 @@
+import fileinput
+import re
+from collections import defaultdict
+from itertools import takewhile
+
+inp = fileinput.input()
+
+crates: dict[int, list[str]] = defaultdict(list)
+for line in takewhile(lambda line: line != "\n", inp):
+    for i in range(1, len(line), 4):
+        if (crate := line[i]).isalpha():
+            crates[i // 4 + 1].append(crate)
+
+for line in inp:
+    n, f, t = [int(s) for s in re.findall(r"\d+", line)]
+    crates[t] = crates[f][n - 1 :: -1] + crates[t]
+    crates[f] = crates[f][n:]
+
+
+print("".join(crates[i][0] for i in range(1, len(crates) + 1)))
