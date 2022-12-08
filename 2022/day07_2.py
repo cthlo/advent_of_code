37ca2009a71195d70ac046b468c829b845df0993
cthlo <cthlo@users.noreply.github.com>
2022-12-08 01:03:32 -0800

    2022 day 7

---
+++ 2022/day07_2.py
@@ -0,0 +1,43 @@
+import fileinput
+from collections import defaultdict
+from itertools import chain
+from typing import Callable
+
+inp = fileinput.input()
+
+
+def takewhile(predicate: Callable[[str], bool]):
+    global inp
+    for line in inp:
+        if predicate(line):
+            yield line
+        else:
+            inp = chain([line], inp)
+            break
+
+
+curr: tuple[str, ...] = ()
+dirs: set[str] = set()
+sizes: defaultdict[tuple[str, ...], int] = defaultdict(int)
+
+while line := next(inp, None):
+    parts = line.split()
+    if parts[1] == "cd":
+        if parts[2] == "..":
+            curr = curr[:-1]
+        elif parts[2] == "/":
+            curr = ()
+        else:
+            curr += (parts[2],)
+    else:
+        for ls_line in takewhile(lambda l: l[0] != "$"):
+            size_or_dir, f = ls_line.split()
+            if size_or_dir == "dir":
+                sizes.setdefault(curr + (f,), 0)
+            elif curr not in dirs:
+                for i in range(len(curr), -1, -1):
+                    sizes[curr[:i]] += int(size_or_dir)
+
+
+need = 30_000_000 + sizes[()] - 70_000_000
+print(next(v for v in sorted(sizes.values()) if v >= need))
